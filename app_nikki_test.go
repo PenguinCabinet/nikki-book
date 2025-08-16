cbe396062f36a8b6d5fe08bfc041e37a057f45cb
PenguinCabinet <kage2.kage22@gmail.com>
2025-08-16 11:19:04 +0900

    Fix test

--- app_nikki_test.go
+++ app_nikki_test.go
@@ -12,6 +12,6 @@ func TestParse_nikki_file(t *testing.T) {
 	app.Setting_data.Fname_format = "YYYY年MM月DD日.txt"
 
 	assert.EqualExportedValues(t,
-		Nikki_t{Fname: "2024年09月19日.txt", Date: Nikki_date_t{Year: 2024, Month: 9, Day: 19}, Content: "Test!"},
+		Nikki_t{Fname: "2024年09月19日.txt", Date: Nikki_date_t{Year: 2024, Month: 9, Day: 19}, Content: "", Is_loading: false},
 		app.Parse_nikki_file("2024年09月19日.txt"))
 }
